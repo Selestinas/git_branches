db70fea0ddbf4cff74e9e758b7d45593a6a3d637
Selestinas <116093514+Selestinas@users.noreply.github.com>
2023-04-05 21:30:51 +0300

    Update hw2_post_method.js

--- hw2_post_method.js
+++ hw2_post_method.js
@@ -1,5 +1,13 @@
-http://162.55.220.72:5005/user_info_3
+http://162.55.220.72:5005/first
+1. Отправить запрос.
+2. Статус код 200
+3. Проверить, что в body приходит правильный string.
+pm.test("Status code is 200", function () {
+    pm.response.to.have.status(200);
+});
+
 //1. Отправить запрос.
+http://162.55.220.72:5005/user_info_3
 //2. Статус код 200
  pm.test("Status code is 200", function () {
  pm.response.to.have.status(200);
